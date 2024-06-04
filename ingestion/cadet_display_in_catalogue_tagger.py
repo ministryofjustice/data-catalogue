f15b50c05e63f77dc009aace4ece4bd6b5d500be
Matt <38562764+LavMatt@users.noreply.github.com>
2024-06-04 14:10:43 +0100

    add dc_ prefix to `display_in_catalogue` tag (#140)
    Co-authored-by: Tom Webber <80110358+tom-webber@users.noreply.github.com>

--- ingestion/cadet_display_in_catalogue_tagger.py
+++ ingestion/cadet_display_in_catalogue_tagger.py
@@ -8,7 +8,7 @@ from datahub.metadata.schema_classes import TagAssociationClass
 def add_display_in_catalogue_tag(entity_urn: str) -> List[TagAssociationClass]:
     """Compute the tags to associate to a given dataset."""
     if "athena_cadet" not in entity_urn:
-        tag_urn = builder.make_tag_urn(tag="display_in_catalogue")
+        tag_urn = builder.make_tag_urn(tag="dc_display_in_catalogue")
         tags = [TagAssociationClass(tag=tag_urn)]
 
         logging.info(f"Tagging dataset {entity_urn} with {tags}.")
