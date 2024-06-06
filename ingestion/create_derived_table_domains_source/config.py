b26cb850e4187368ec0dddeaa0fc28cb8a032c45
Murdo Moyse <murdo.moyse@digital.justice.gov.uk>
2024-06-06 09:12:52 +0100

    Removed reference to local manifest file

--- ingestion/create_derived_table_domains_source/config.py
+++ ingestion/create_derived_table_domains_source/config.py
@@ -3,9 +3,6 @@ from pydantic import Field
 
 
 class CreateDerivedTableDomainsConfig(ConfigModel):
-    manifest_local_path: str = Field(
-        description="local file path to dbt manifest json", default=None
-    )
     manifest_s3_uri: str = Field(
         description="s3 path to dbt manifest json", default=None
     )
