3f11eb6766c08a140f7eadccc809bda2c2ebc574
Matt <38562764+LavMatt@users.noreply.github.com>
2024-07-18 13:56:34 +0100

    tag transformer for justice data (#205)
    * tag transformer for justice data
    
    * rename of transformer script
    
    * up duration for assuming role

--- ingestion/taggers/display_in_catalogue_tagger.py
+++ ingestion/taggers/display_in_catalogue_tagger.py
@@ -6,7 +6,10 @@ from datahub.metadata.schema_classes import TagAssociationClass
 
 
 def add_display_in_catalogue_tag(entity_urn: str) -> List[TagAssociationClass]:
-    """Compute the tags to associate to a given dataset."""
+    """
+    Transformer to add dc_display_in_catalogue tag to all ingested entities
+    other than the athena entities created via a dbt cadet ingestion
+    """
     if "athena_cadet" not in entity_urn:
         tag_urn = builder.make_tag_urn(tag="dc_display_in_catalogue")
         tags = [TagAssociationClass(tag=tag_urn)]
