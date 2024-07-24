412ad6869d6fde1e205c4d518c413078e5591fa8
LavMatt <matthew.laverty@justice.gov.uk>
2024-07-24 15:54:41 +0100

    add domains to list_all() client method

--- ingestion/justice_data_source/api_client.py
+++ ingestion/justice_data_source/api_client.py
@@ -1,4 +1,20 @@
 import requests
+from ..dbt_manifest_utils import format_domain_name
+
+
+# These map the api ids to domains as set by create_cadet_database_source
+ID_TO_DOMAIN_MAPPING = {
+    "prisons": "prison",
+    "probation": "probation",
+    "courts": "courts",
+    "electronic-monitoring": "electronic monitoring",
+    "electronic-monitoring-performance": "electronic monitoring",
+    "bass": "probation",
+    "cjs-crime": "general",
+    "cjs-reoffending": "general",
+    "cjs-sentence-types": "courts",
+    "cjs-entrants": "courts",
+}
 
 
 class JusticeDataAPIClient:
@@ -15,11 +31,27 @@ class JusticeDataAPIClient:
         to_process.extend(self.session.get(self.base_url).json()["children"])
         while to_process:
             current = to_process.pop()
+            id = current.get("id")
+
+            # we are not ingesting justice-in-numbers - mainly repeated measures from other
+            # sections, plus a couple which are difficult to assign a domain as they're not MOJ
+            if id == "justice-in-numbers":
+                continue
+
+            if ID_TO_DOMAIN_MAPPING.get(id):
+                domain = format_domain_name(ID_TO_DOMAIN_MAPPING.get(id, ""))
+            elif not current.get("is_child", False):
+                domain = None
+
+            current["domain"] = domain
             breadcrumb = current.get("breadcrumb", []).copy()
             breadcrumb.append(current["name"])
+
             if current["children"] and current["children"] != [None]:
                 for child in current["children"]:
                     child["breadcrumb"] = breadcrumb
+                    child["is_child"] = True
+                    child["domain"] = domain
 
                 to_process.extend(current["children"])
             else:
