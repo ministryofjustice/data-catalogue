f2a8280cf73d86427d9863816abe245d6e88c5a6
LavMatt <matthew.laverty@justice.gov.uk>
2024-07-25 17:00:33 +0100

    move ID_TO_DOMAIN_MAPPING to config and add exclude_id_list to JusticeDataAPIConfig

--- ingestion/justice_data_source/config.py
+++ ingestion/justice_data_source/config.py
@@ -1,6 +1,25 @@
 from datahub.configuration.common import ConfigModel
 from pydantic import Field
 
+# These map the api ids to domains as set by create_cadet_database_source
+# and all children of these ids will inherit their parent's domain
+ID_TO_DOMAIN_MAPPING = {
+    "prisons": "prison",
+    "probation": "probation",
+    "courts": "courts",
+    "electronic-monitoring": "electronic monitoring",
+    "electronic-monitoring-performance": "electronic monitoring",
+    "bass": "probation",
+    "cjs-crime": "general",
+    "cjs-reoffending": "general",
+    "cjs-sentence-types": "courts",
+    "cjs-entrants": "courts",
+}
+
 
 class JusticeDataAPIConfig(ConfigModel):
     base_url: str = Field(description="URL to justice data API")
+    exclude_id_list: list[str] = Field(
+        description="list of ids to exclude from the ingestion, inclusive of that id and all children",
+        default=[],
+    )
