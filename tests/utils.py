50553d69ca9fc986c138f79141b16ea5249f80f6
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2024-11-05 10:26:48 +0000

    Configure ContainerTransformer subclass and update tests

--- tests/utils.py
+++ tests/utils.py
@@ -4,13 +4,30 @@ import datahub.emitter.mce_builder as builder
 import datahub.metadata.schema_classes as models
 from datahub.emitter.mcp import MetadataChangeProposalWrapper
 from datahub.ingestion.api.common import EndOfStream, PipelineContext, RecordEnvelope
-from datahub.ingestion.transformer.dataset_transformer import DatasetTransformer
+from datahub.ingestion.transformer.dataset_transformer import (
+    DatasetTransformer,
+    ContainerTransformer,
+)
 from datahub.metadata.schema_classes import MetadataChangeEventClass
 from datahub.utilities.urns._urn_base import Urn
 
 
 def make_generic_dataset_mcp(
     entity_urn: str = "urn:li:dataset:(urn:li:dataPlatform:dbt,cadet.awsdatacatalog.prison_database.table1,PROD)",
+    aspect_name: str = "status",
+    aspect: Any = models.StatusClass(removed=False),
+) -> MetadataChangeProposalWrapper:
+    return MetadataChangeProposalWrapper(
+        entityUrn=entity_urn,
+        entityType=Urn.from_string(entity_urn).entity_type,
+        aspectName=aspect_name,
+        changeType="UPSERT",
+        aspect=aspect,
+    )
+
+
+def make_generic_mcp(
+    entity_urn: str = "urn:li:container:MOJDF_magistrates",
     aspect_name: str = "status",
     aspect: Any = models.StatusClass(removed=False),
 ) -> MetadataChangeProposalWrapper:
@@ -51,6 +68,31 @@ def run_dataset_transformer_pipeline(
     outputs = list(
         transformer.transform(
             [RecordEnvelope(input, metadata={}) for input in [dataset, EndOfStream()]]
+        )
+    )
+    return outputs
+
+
+def run_container_transformer_pipeline(
+    transformer_type: Type[ContainerTransformer],
+    aspect: Optional[builder.Aspect],
+    config: dict,
+    pipeline_context: PipelineContext = PipelineContext(run_id="transformer_pipe_line"),
+) -> List[RecordEnvelope]:
+    transformer: ContainerTransformer = cast(
+        ContainerTransformer, transformer_type.create(config, pipeline_context)
+    )
+
+    assert aspect
+    container = make_generic_mcp(
+        entity_urn="urn:li:container:abc",
+        aspect=aspect,
+        aspect_name=transformer.aspect_name(),
+    )
+
+    outputs = list(
+        transformer.transform(
+            [RecordEnvelope(input, metadata={}) for input in [container, EndOfStream()]]
         )
     )
     return outputs
