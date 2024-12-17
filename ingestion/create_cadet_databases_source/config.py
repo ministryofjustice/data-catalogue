ef59383792d1ee8ac54da6164154c445a3433950
Mitch Dawson <86007219+mitchdawson1982@users.noreply.github.com>
2024-12-17 13:34:54 +0000

    Add stateful ingestion to create cadet databases (#396)
    * Update base classes and add get_wprkunit_processors method to CreateCadetDatabases
    
    * Add pipeline name and staeful ingestion params
    
    * Add code to link database and tables
    
    * remove additional code to link databases and tables

--- ingestion/create_cadet_databases_source/config.py
+++ ingestion/create_cadet_databases_source/config.py
@@ -1,11 +1,25 @@
-from datahub.configuration.common import ConfigModel
+from typing import Optional
+
+from datahub.ingestion.source.state.stale_entity_removal_handler import (
+    StatefulStaleMetadataRemovalConfig,
+)
+from datahub.ingestion.source.state.stateful_ingestion_base import (
+    StatefulIngestionConfigBase,
+)
 from pydantic import Field
 
 
-class CreateCadetDatabasesConfig(ConfigModel):
+class CreateCadetDatabasesConfig(StatefulIngestionConfigBase):
     manifest_s3_uri: str = Field(
         description="s3 path to dbt manifest json", default=None
     )
     database_metadata_s3_uri: str = Field(
         description="s3 path to database_metadata json", default=None
+    )
+    stateful_ingestion: Optional[StatefulStaleMetadataRemovalConfig] = Field(
+        description="""
+            Can configure whether the ingestion is be be staeful and can remove stale metadata.
+            see https://datahubproject.io/docs/metadata-ingestion/docs/dev_guides/stateful/#stale-entity-removal
+            """,
+        default=None,
     )
