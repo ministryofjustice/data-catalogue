f15fba3db238232052cac6d33ba36f579171e0e8
Murdo Moyse <murdo.moyse@digital.justice.gov.uk>
2025-01-03 13:45:04 +0000

    refactor: remove commented-out domain tag creation code

--- ingestion/create_cadet_databases_source/source.py
+++ ingestion/create_cadet_databases_source/source.py
@@ -216,19 +216,8 @@ class CreateCadetDatabases(StatefulIngestionSourceBase):
                 entityUrn=dataset_urn,
                 aspect=DomainsClass(domains=[domain_urn]),
             )
-
-            # Create domain tag for the table
-            # domain_tag = mce_builder.make_tag_urn(tag=domain_name)
-            # logging.info(f"Creating domain tag {domain_name=}")
-            # create_domain_tag_mcp = MetadataChangeProposalWrapper(
-            #     entityType="dataset",
-            #     changeType=ChangeTypeClass.UPSERT,
-            #     entityUrn=dataset_urn,
-            #     aspect=GlobalTagsClass(tags=[TagAssociationClass(tag=domain_tag)]),
-            # )
-
             table_domain_mcps.append(mcp)
-            # table_domain_mcps.append(create_domain_tag_mcp)
+
         return table_domain_mcps
 
     def _get_domains(self, manifest) -> set[str]:
