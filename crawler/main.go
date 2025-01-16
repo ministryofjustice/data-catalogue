6deebd34ab887a0d46e2f24e96b10373fbb56bdf
Helder Ribeiro <helder.ribeiro@digital.justice.gov.uk>
2025-01-16 11:39:57 +0000

    fmd-778 - Remove unused mutex and Add support to check external links
    Signed-off-by: Helder Ribeiro <helder.ribeiro@digital.justice.gov.uk>

--- crawler/main.go
+++ crawler/main.go
@@ -3,31 +3,35 @@ package main
 import (
 	"flag"
 	"fmt"
+	"golang.org/x/net/html"
 	"io"
 	"net/http"
 	"net/url"
-	"sync"
-
-	"golang.org/x/net/html"
+	"strings"
 )
 
 type Crawler struct {
-	visited      map[string]bool
-	brokenLinks  int
-	brokenReport []string
-	mu           sync.Mutex
+	visited          map[string]bool
+	brokenLinks      int
+	brokenReport     []string
+	brokenSources    map[string]string
+	secondaryDomains map[string]bool
 }
 
 func NewCrawler() *Crawler {
 	return &Crawler{
-		visited:      make(map[string]bool),
-		brokenReport: []string{},
+		visited:       make(map[string]bool),
+		brokenReport:  []string{},
+		brokenSources: map[string]string{},
+		secondaryDomains: map[string]bool{
+			"data.justice.gov.uk": true,
+			"www.gov.uk":          true,
+			"criminal-justice-delivery-data-dashboards.justice.gov.uk": true,
+		},
 	}
 }
 
 func (c *Crawler) Visit(link string) bool {
-	c.mu.Lock()
-	defer c.mu.Unlock()
 	if c.visited[link] {
 		return false
 	}
@@ -35,22 +39,25 @@ func (c *Crawler) Visit(link string) bool {
 	return true
 }
 
-func (c *Crawler) ReportBrokenLink(link string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+func (c *Crawler) ReportBrokenLink(link string, source string) {
 	c.brokenLinks++
 	c.brokenReport = append(c.brokenReport, link)
+	c.brokenSources[link] = source
+
 }
 
-func (c *Crawler) Crawl(domain string, link string, depth int) {
+func (c *Crawler) Crawl(domain string, link string, source string, depth int) {
 	if depth <= 0 {
+		return
+	}
+
+	if strings.HasPrefix(link, "http://127.0.0.1:8000/feedback/") {
 		return
 	}
 
 	if !c.Visit(link) {
 		return
 	}
-	fmt.Println("Visiting:", link)
 
 	parsedURL, err := url.Parse(link)
 	if err != nil {
@@ -58,35 +65,45 @@ func (c *Crawler) Crawl(domain string, link string, depth int) {
 		return
 	}
 
+	// Skip links outside the main domain and secondary domains
+	if parsedURL.Host != domain && !c.secondaryDomains[parsedURL.Host] {
+		return
+	}
+
+	fmt.Println("Visiting:", link)
+
 	// Properly encode query parameters
 	parsedURL.RawQuery = parsedURL.Query().Encode()
 
-	encodedLink := parsedURL.String() // Fully encoded URL
-	fmt.Println("Encoded URL:", encodedLink)
-
-	resp, err := http.Get(encodedLink)
-
+	resp, err := http.Get(link)
 	if err != nil {
 		fmt.Println("Error fetching link:", link, err)
-		c.ReportBrokenLink(link)
+		c.ReportBrokenLink(link, source)
 		return
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		fmt.Println("Non-OK HTTP status:", resp.StatusCode, "for link:", link)
-		c.ReportBrokenLink(link)
+		c.ReportBrokenLink(link, source)
 		return
 	}
 
+	// Stop crawling further for secondary domains
+	if c.secondaryDomains[parsedURL.Host] {
+		fmt.Println("Checked link within secondary domain:", link)
+		return
+	}
+
+	// Extract and crawl links within the main domain
 	links := extractLinks(resp.Body, domain)
 	for _, l := range links {
-		c.Crawl(domain, l, depth-1)
+		c.Crawl(domain, l, link, depth-1)
 	}
 }
 
 func extractLinks(body io.Reader, domain string) []string {
-	links := []string{}
+	var links []string
 	page := html.NewTokenizer(body)
 
 	for {
@@ -117,6 +134,11 @@ func resolveLink(href, domain string) string {
 		return ""
 	}
 
+	// Ignore non-HTTP(S) schemes like mailto:
+	if parsedLink.Scheme != "" && parsedLink.Scheme != "http" && parsedLink.Scheme != "https" {
+		return ""
+	}
+
 	if parsedLink.Scheme == "" {
 		parsedLink.Scheme = "http"
 	}
@@ -125,16 +147,23 @@ func resolveLink(href, domain string) string {
 		parsedLink.Host = domain
 	}
 
-	if parsedLink.Host != domain {
-		return ""
-	}
+	// Normalize URL: Remove fragments and trailing slashes
+	parsedLink.Fragment = ""
+	parsedLink.Path = normalizePath(parsedLink.Path)
 
 	return parsedLink.String()
 }
 
+func normalizePath(path string) string {
+	if len(path) > 1 && path[len(path)-1] == '/' {
+		return path[:len(path)-1]
+	}
+	return path
+}
+
 func main() {
 	defaultURL := "http://127.0.0.1:8000"
-	defaultDepth := 5
+	defaultDepth := 7
 
 	startURL := flag.String("url", defaultURL, "Starting URL for the crawler")
 	depth := flag.Int("depth", defaultDepth, "Depth to crawl")
@@ -148,13 +177,13 @@ func main() {
 
 	domain := parsedURL.Host
 	crawler := NewCrawler()
-	crawler.Crawl(domain, *startURL, *depth)
+	crawler.Crawl(domain, *startURL, *startURL, *depth)
 
 	fmt.Printf("Broken links encountered: %d\n", crawler.brokenLinks)
 	if crawler.brokenLinks > 0 {
-		fmt.Println("Broken links:")
-		for _, link := range crawler.brokenReport {
-			fmt.Println("-", link)
+		fmt.Println("Broken links and their sources:")
+		for link, source := range crawler.brokenSources {
+			fmt.Printf("- %s (found on: %s)\n", link, source)
 		}
 	}
 }
