1c9d7109da486ce7ce9130dec733152b344c79c8
LavMatt <matthew.laverty@justice.gov.uk>
2025-01-29 21:34:52 +0000

    change default get from mapping to empty dict

--- ingestion/transformers/assign_cadet_databases.py
+++ ingestion/transformers/assign_cadet_databases.py
@@ -110,7 +110,7 @@ class AssignCadetDatabases(DatasetTransformer, metaclass=ABCMeta):
         logging.debug("Assigning datasets to databases")
         print(f"{self.entity_map.keys()=}")
         for dataset_urn in self.entity_map.keys():
-            container_urn = self.mappings.get(dataset_urn).get("database")
+            container_urn = self.mappings.get(dataset_urn, {}).get("database")
             if not container_urn:
                 logging.warning(f"No container mapping for {dataset_urn=}")
                 continue
