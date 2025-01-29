39259d1960faad6c2bfe547508708334889e22d8
Murdo Moyse <murdo.moyse@digital.justice.gov.uk>
2025-01-29 13:50:52 +0000

    Add CADET_INSTANCE input to workflows and update config to use environment variable

--- ingestion/config.py
+++ ingestion/config.py
@@ -1,5 +1,10 @@
+import logging
+import os
+
 PLATFORM = "dbt"
 # this needs to match the platform_instance value in cadet.yaml dbt recipe
 # minus the .awscatalog bit
-INSTANCE = "cadet.awsdatacatalog"
+INSTANCE = os.getenv("CADET_INSTANCE")
+if not INSTANCE:
+    logging.error("CADET_INSTANCE not set in github workflows")
 ENV = "PROD"
