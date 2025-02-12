dc8799783d93d044619cce9865034d52c6888bff
Mat Moore <MatMoore@users.noreply.github.com>
2025-02-12 12:34:37 +0000

    refactor: create WorkunitInspector helper for tests

--- tests/create_cadet_databases/test_source.py
+++ tests/create_cadet_databases/test_source.py
@@ -2,7 +2,7 @@ from datahub.ingestion.api.common import PipelineContext
 from datahub.ingestion.source.state.stale_entity_removal_handler import (
     StatefulStaleMetadataRemovalConfig,
 )
-from utils import extract_tag_names, group_metadata
+from utils import WorkunitInspector
 
 from ingestion.create_cadet_databases_source.source import (
     CreateCadetDatabases,
@@ -22,28 +22,31 @@ def run_source(mock_datahub_graph):
         ctx=PipelineContext(run_id="abc", graph=mock_datahub_graph),
     )
 
-    return group_metadata(source.get_workunits())
+    return WorkunitInspector(source.get_workunits())
 
 
 def test_tags(mock_datahub_graph):
     metadata = run_source(mock_datahub_graph)
 
-    courts_data_tags = extract_tag_names(
-        metadata["urn:li:container:1e7a7a180ed4f1215bff62f4ce93993e"]["globalTags"]
-    )
-    probation_database_tags = extract_tag_names(
-        metadata["urn:li:container:ea9744b8004d93b716687bab12438c90"]["globalTags"]
-    )
-    prison_database_tags = extract_tag_names(
-        metadata["urn:li:container:b17e173b8950dee2415a3119fb7c9d12"]["globalTags"]
-    )
+    courts_data_tags = metadata.entity(
+        "urn:li:container:1e7a7a180ed4f1215bff62f4ce93993e"
+    ).tag_names
 
-    ref_database_tags = extract_tag_names(
-        metadata["urn:li:container:27c5c4df57bf429bf9e56e51b30003ed"]["globalTags"]
-    )
-    hq_database_tags = extract_tag_names(
-        metadata["urn:li:container:48e5e41ce461da41f0333b67a322fb99"]["globalTags"]
-    )
+    probation_database_tags = metadata.entity(
+        "urn:li:container:ea9744b8004d93b716687bab12438c90"
+    ).tag_names
+
+    prison_database_tags = metadata.entity(
+        "urn:li:container:b17e173b8950dee2415a3119fb7c9d12"
+    ).tag_names
+
+    ref_database_tags = metadata.entity(
+        "urn:li:container:27c5c4df57bf429bf9e56e51b30003ed"
+    ).tag_names
+
+    hq_database_tags = metadata.entity(
+        "urn:li:container:48e5e41ce461da41f0333b67a322fb99"
+    ).tag_names
 
     assert set(courts_data_tags) == {
         "urn:li:tag:Courts and tribunals",
