61b72a7053470e202b2aefdbbbd797002e90bc9a
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2026-02-03 11:54:26 +0000

    Refactor tests for AddOwnership, AddProperties, and AddTag transformers to enhance overwrite functionality

--- tests/test_enrich_container_transformer.py
+++ tests/test_enrich_container_transformer.py
@@ -1,3 +1,4 @@
+from typing import cast
 from unittest.mock import MagicMock
 
 import datahub.emitter.mcp_builder as mcp_builder
@@ -13,61 +14,145 @@ from ingestion.transformers.enrich_container_transformer import (
     EnrichContainerTransformer,
 )
 
+from ingestion.transformers.enrich_container import (
+    AddOwnershipTransformer, 
+    AddOwnershipTransformerConfig, 
+    AddTagTransformer,
+    AddTagTransformerConfig,
+    AddPropertiesTransformer,
+    AddPropertiesTransformerConfig
+)
 
-class TestEnrichContainerTransformer:
-    def test_pattern_add_dataset_domain_match(self, mock_datahub_graph):
-
-        pipeline_context: PipelineContext = PipelineContext(run_id="abc")
-        graph = mock_datahub_graph(DatahubClientConfig)
-        graph.get_aspect = MagicMock(
-            return_value=ContainerPropertiesClass(name="foo", customProperties=None)
-        )
-        pipeline_context.graph = graph
-        expected_key = mcp_builder.DatabaseKey(
-            database="prison_database",
-            platform=PLATFORM,
-            instance=INSTANCE,
-            env=ENV,
-            backcompat_env_as_instance=True,
-        )
-
-        output = run_container_transformer_pipeline(
-            transformer_type=EnrichContainerTransformer,
-            aspect=models.ContainerClass(container=expected_key.database),
-            config={
-                "data_custodian": "urn:li:corpuser:roy.keane",
-                "subject_areas": ["Prison"],
-                "properties": {"security_classification": "Official-Sensitive"},
-            },
-            pipeline_context=pipeline_context,
+class TestAddOwnershipTransformer:
+    def test_simple_add_ownership_transformer_overwrite(self):
+        transformer = AddOwnershipTransformer(
+            config=AddOwnershipTransformerConfig(
+                data_custodian="urn:li:corpuser:test.user"
+            ),
+            ctx=PipelineContext(run_id="test_run"),
         )
 
-        assert len(output) == 4
-
-        results = {}
-        for o in output[:-1]:
-            results[o.record.aspect.ASPECT_NAME] = o.record
-
-        for v in results.values():
-            assert isinstance(v, MetadataChangeProposalWrapper)
-            assert v.entityType == "container"
-            assert v.changeType == "UPSERT"
-            assert v.entityUrn == "urn:li:container:abc"
-
-        assert isinstance(results["ownership"].aspect, models.OwnershipClass)
-        assert (
-            results["ownership"].aspect.owners[0].owner == "urn:li:corpuser:roy.keane"
-        )
-        assert results["ownership"].aspect.owners[0].type == "DATAOWNER"
-
-        assert isinstance(results["globalTags"].aspect, models.GlobalTagsClass)
-        assert (
-            results["globalTags"].aspect.tags[0].tag
-            == "urn:li:tag:dc_display_in_catalogue"
+        aspect = transformer.transform_aspect(
+            entity_urn="urn:li:container:test_container",
+            aspect_name="ownership",
+            aspect=None,
         )
 
-        assert results["containerProperties"]
+        assert isinstance(aspect, models.OwnershipClass)
+        assert len(aspect.owners) == 1
+        assert aspect.owners[0].owner == "urn:li:corpuser:test.user"
+        assert aspect.owners[0].type == "DATAOWNER"
 
-        assert results["containerProperties"].aspect.customProperties == {
-            "security_classification": "Official-Sensitive"
-        }
+class TestAddPropertiesTransformer:
+    def test_add_properties_transformer_overwrite_description_and_properties(self):
+        pipeline_context: PipelineContext = PipelineContext(run_id="test_run")
+        transformer = AddPropertiesTransformer(
+            config=AddPropertiesTransformerConfig(
+                description="Test container description",
+                properties={"key1": "value1", "key2": "value2"},
+            ),
+            ctx=pipeline_context,
+        )
+
+        container_properties: ContainerPropertiesClass = ContainerPropertiesClass(
+            name="test_container",
+            description="old description",
+            customProperties={"old_key": "old_value"},
+        )
+
+        aspect = transformer.transform_aspect(
+            entity_urn="urn:li:container:test_container",
+            aspect_name="containerProperties",
+            aspect=container_properties,
+        )
+
+        assert aspect
+        assert isinstance(aspect, ContainerPropertiesClass)
+        assert aspect.name == "test_container"
+        assert aspect.description == "Test container description"
+        assert aspect.customProperties == {"key1": "value1", "key2": "value2"}
+    
+    def test_add_properties_transformer_overwrite_properties(self):
+        pipeline_context: PipelineContext = PipelineContext(run_id="test_run")
+        transformer = AddPropertiesTransformer(
+            config=AddPropertiesTransformerConfig(
+                properties={"key1": "value1", "key2": "value2"},
+            ),
+            ctx=pipeline_context,
+        )
+
+        container_properties: ContainerPropertiesClass = ContainerPropertiesClass(
+            name="test_container",
+            description="old description",
+            customProperties={"old_key": "old_value"},
+        )
+
+        aspect = transformer.transform_aspect(
+            entity_urn="urn:li:container:test_container",
+            aspect_name="containerProperties",
+            aspect=container_properties,
+        )
+
+        assert aspect
+        assert isinstance(aspect, ContainerPropertiesClass)
+        assert aspect.name == "test_container"
+        assert aspect.description == "old description"
+        assert aspect.customProperties == {"key1": "value1", "key2": "value2"}
+
+    def test_add_properties_transformer_overwrite_description(self):
+        pipeline_context: PipelineContext = PipelineContext(run_id="test_run")
+        transformer = AddPropertiesTransformer(
+            config=AddPropertiesTransformerConfig(
+                description="Test container description",
+            ),
+            ctx=pipeline_context,
+        )
+
+        container_properties: ContainerPropertiesClass = ContainerPropertiesClass(
+            name="test_container",
+            description="old description",
+            customProperties={"old_key": "old_value"},
+        )
+
+        aspect = transformer.transform_aspect(
+            entity_urn="urn:li:container:test_container",
+            aspect_name="containerProperties",
+            aspect=container_properties,
+        )
+
+        assert aspect
+        assert isinstance(aspect, ContainerPropertiesClass)
+        assert aspect.name == "test_container"
+        assert aspect.description == "Test container description"
+        assert aspect.customProperties == {"old_key": "old_value"}
+
+class TestAddTagTransformer:
+    def test_add_tag_transformer_overwrite(self):
+        pipeline_context: PipelineContext = PipelineContext(run_id="test_run")
+        transformer = AddTagTransformer(
+            config=AddTagTransformerConfig(
+                tag_urns=[
+                    "urn:li:tag:tag1",
+                    "urn:li:tag:tag2",
+                ],
+            ),
+            ctx=pipeline_context,
+        )
+
+        global_tags: models.GlobalTagsClass = models.GlobalTagsClass(
+            tags=[
+                models.TagAssociationClass(tag="urn:li:tag:old_tag"),
+            ]
+        )
+
+        aspect = transformer.transform_aspect(
+            entity_urn="urn:li:container:test_container",
+            aspect_name="globalTags",
+            aspect=global_tags,
+        )
+
+        assert aspect
+        assert isinstance(aspect, models.GlobalTagsClass)
+        assert len(aspect.tags) == 2
+        assert aspect.tags[0].tag == "urn:li:tag:tag1"
+        assert aspect.tags[1].tag == "urn:li:tag:tag2"
